47e8bb289eebc63e1c1b3ff48b148cc0d5495ade
kohheepeace <kohheepeace@gmail.com>
2021-08-27 13:14:57 +0900

    add config to find correct webpack-dev-server url in gitpod

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,22 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+const environment = require("./environment");
+const { merge } = require("webpack-merge");
 
-const environment = require('./environment')
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+const defaultConfig = environment.toWebpackConfig();
 
-module.exports = environment.toWebpackConfig()
+if (process.env.GITPOD_WORKSPACE_URL) {
+	const [protocol, hostname] = process.env.GITPOD_WORKSPACE_URL.split("://");
+
+	// ex: https://3035-fuchsia-lamprey-16r2m3h5.ws-us14.gitpod.io
+	const devServerPublic = `https://3035-${hostname}`;
+
+	const mergedConfig = merge(defaultConfig, {
+		devServer: {
+			public: devServerPublic,
+		},
+	});
+
+	module.exports = mergedConfig;
+} else {
+	module.exports = defaultConfig;
+}
