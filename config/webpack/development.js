fec558d6c959b7ffaee5795cb7773a30d02a51dd
EGaroufalias <eliasgaroufalias@gmail.com>
2022-09-22 15:26:56 +0000

    First commit

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,22 +1,5 @@
-const environment = require("./environment");
-const { merge } = require("webpack-merge");
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
-const defaultConfig = environment.toWebpackConfig();
+const environment = require('./environment')
 
-if (process.env.GITPOD_WORKSPACE_URL) {
-	const [protocol, hostname] = process.env.GITPOD_WORKSPACE_URL.split("://");
-
-	// ex: https://3035-fuchsia-lamprey-16r2m3h5.ws-us14.gitpod.io
-	const devServerPublic = `https://3035-${hostname}`;
-
-	const mergedConfig = merge(defaultConfig, {
-		devServer: {
-			public: devServerPublic,
-		},
-	});
-
-	module.exports = mergedConfig;
-} else {
-	module.exports = defaultConfig;
-}
+module.exports = environment.toWebpackConfig()
